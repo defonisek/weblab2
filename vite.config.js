13e30f6191b1d972744566bd255bbdd0d6f68189
defonisek <defonisek@mail.ru>
2025-10-30 13:22:48 +0300

    tbr

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
+import { defineConfig } from 'vite'
+import { resolve } from 'path'
+
 export default {
   base: './',
   build: {
     outDir: 'dist',
-    assetsDir: 'assets'
-  }
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        churchCalendar: resolve(__dirname, 'src/html/church-calendar.html'),
+        europeana: resolve(__dirname, 'src/html/europeana.html'),
+        facts: resolve(__dirname, 'src/html/facts.html'),
+        weather: resolve(__dirname, 'src/html/weather.html')
+      }
+    }
 }
